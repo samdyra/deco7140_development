eb8a59204de87c236c1f7edec95238d1b31ecf25
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2025-08-06 19:13:09 +1000

    feat: independent A2

---
+++ work_phase_1/js/components.js
@@ -0,0 +1,30 @@
+/**
+* IMPORTS
+* Keep track of external modules being used
+* */
+import { logMessage } from "./modules/logging.js"
+
+/**
+* CONSTANTS
+* Define values that don't change e.g. page titles, URLs, etc.
+* */
+const PAGE_NAME = "components.js"
+
+/**
+* VARIABLES
+* Define values that will change e.g. user inputs, counters, etc.
+* */
+let message = "Page has fully loaded"
+
+/**
+* FUNCTIONS
+* Group code into functions to make it reusable
+* */
+
+/**
+* EVENT LISTENERS
+* The code that runs when a user interacts with the page
+* */
+window.addEventListener("load", () => {
+    logMessage(PAGE_NAME, message);
+})
---
+++ work_phase_1/js/modules/logging.js
@@ -0,0 +1,5 @@
+function logMessage(pageName, message) {
+    console.log(`From ${pageName}: ${message}`)
+}
+
+export { logMessage }
