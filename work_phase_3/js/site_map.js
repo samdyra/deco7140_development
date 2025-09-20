1b6b20ba5f3ef0cf0d0826ca3a49d9bbfc1ae9cb
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2025-09-20 14:57:20 +1000

    feat: adding sitemap image in sitemap page

---
+++ work_phase_3/js/site_map.js
@@ -0,0 +1,24 @@
+/**
+* IMPORTS
+* Keep track of external modules being used
+* */
+
+/**
+* CONSTANTS
+* Define values that don't change e.g. page titles, URLs, etc.
+* */
+
+/**
+* VARIABLES
+* Define values that will change e.g. user inputs, counters, etc.
+* */
+
+/**
+* FUNCTIONS
+* Group code into functions to make it reusable
+* */
+
+/**
+* EVENT LISTENERS
+* The code that runs when a user interacts with the page
+* */
