29fc8b6d6b199e8e6a1d464cfd55782468c75d21
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2025-10-21 11:56:08 +1000

    feat: add map container

---
+++ work_phase_4/js/crumbs_map.js
@@ -0,0 +1,36 @@
+/**
+* IMPORTS
+* Keep track of external modules being used
+* */
+
+/**
+* CONSTANTS
+* Define values that don't change e.g. page titles, URLs, etc.
+* */
+
+/**
+* VARIABLES
+* Define values that will change e.g. user inputs, counters, etc.
+* */
+
+/**
+* FUNCTIONS
+* Group code into functions to make it reusable
+* */
+
+/**
+* EVENT LISTENERS
+* The code that runs when a user interacts with the page
+* */
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById('mapContainer');
+
+    /* Scroll to center the map image
+    scrollWidth/ScrollHeight = total width/height of the image
+    lientWidth/clientHeight = width/height of the visible area
+    scrollHeight - clientHeight = total scrollable height, similarly for width
+    Dividing by 2 centers the scroll position    
+     */
+    container.scrollLeft = (container.scrollWidth - container.clientWidth) / 2;
+    container.scrollTop = (container.scrollHeight - container.clientHeight) / 2;
+});
